28df6c73aef6252a2382093c9ce9e4da191a6efa
adamdasovich <adamdasovich@gmail.com>
2022-09-19 13:26:25 -0400

    initiating

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+
+
+const App = () => {
+	return (
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Routes>
+						<Route path='/login/shipping' element={<ShippingScreen />} />
+						<Route path='/login' element={<LoginScreen />} />
+						<Route path='/register' element={<RegisterScreen />} />
+						<Route path='/product/:id' element={<ProductScreen />} />
+						<Route path='/cart/:id' element={<CartScreen />} />
+						<Route path='/' element={<HomeScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+}
+
+export default App;
